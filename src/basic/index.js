93fed9bced7f2cebb65b0982144aae692ee4bc69
Igor Cezatte <igorcezatte13@gmail.com>
2020-01-17 22:44:31 -0300

    Screenshot png

---
+++ src/basic/index.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  })
+  await page.goto('https://github.com/igorcezatte');
+  await page.screenshot({ path: 'gitpage.png' });
+
+  await browser.close();
+})();
